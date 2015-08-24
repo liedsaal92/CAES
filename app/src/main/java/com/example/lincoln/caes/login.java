3e4e457b76daeeb21e77df35bd4f52e63be9e4e6
Lincoln <liedsaal92@hotmail.es>
2015-08-24 11:02:29 -0500

    prueba de cambio

--- app/src/main/java/com/example/lincoln/caes/login.java
+++ app/src/main/java/com/example/lincoln/caes/login.java
@@ -19,7 +19,7 @@ public class login extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
-
+//
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
